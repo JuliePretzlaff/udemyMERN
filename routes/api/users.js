caef0a4b7afb5efe49b3f24731aa7bb3d1878cf7
JuliePretzlaff <Juliapretzlaff@gmail.com>
2018-11-07 20:13:13 -0800

    user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,31 @@ router.post('/register', (req, res) => {
         })
 })
 
+// @route   GET api/users/login
+// @desc    login users route/ return JWT token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email:
+    User.findOne({ email })
+        .then(user => {
+            // check for user
+            if(!user) {
+                return res.status(404).json({email: 'User not found.'});
+            }
+
+            // chack password:
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({password: 'Incorrect password.'});
+                    }
+                })
+        });
+});
+
 module.exports = router;
